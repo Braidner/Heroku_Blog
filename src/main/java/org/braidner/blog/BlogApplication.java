98a31d4e344d33c7b44ecc082aedb93e2957639d
braidner <goodly92@gmail.com>
2015-10-24 22:13:39 +0300

    hystrix update

--- src/main/java/org/braidner/blog/BlogApplication.java
+++ src/main/java/org/braidner/blog/BlogApplication.java
@@ -2,6 +2,8 @@ package org.braidner.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,6 +12,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableAspectJAutoProxy
 @EnableJpaAuditing
 @EnableJpaRepositories
+@EnableHystrix
+@EnableHystrixDashboard
 public class BlogApplication {
 
     public static void main(String[] args) {
